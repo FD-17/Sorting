5f40bb68cf1d16b8ac8087092559a4841a8871ca
FD-17 <54020957+FD-17@users.noreply.github.com>
2019-08-31 14:21:37 +0530

    Create Advanced_bubble_sort.java

---
+++ Advanced_bubble_sort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Advanced_bubble_sort
+{
+    static int[] Bubble(int[] a){
+        for(int i=0;i<a.length-1;i++){
+            //swap variable to break out of loop if array is of sorted elements
+            boolean swap=false;
+            for(int j=0;j<a.length-i-1;j++){
+                if(a[j]>a[j+1])
+			//swapping a[j] and a[j+1]
+                a[j+1]=(a[j+1]+a[j])-(a[j]=a[j+1]);
+                swap=true;
+            }
+            if(swap==false)
+            break;
+        }return a;
+    }
+	public static void main(String[] args) {
+		//Getting user input
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int arr[]=new int[n];
+	    for(int i=0;i<n;i++){
+	        arr[i]=sc.nextInt();
+	    }
+		System.out.println(Arrays.toString(Bubble(arr)));
+	}
+}
